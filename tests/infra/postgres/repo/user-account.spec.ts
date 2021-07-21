acd454936da5180f543435f338f80d7ae06eda5f
Alyson Souza <alysonforever@gmail.com>
2021-07-21 09:26:05 -0300

    ensure PgUserAccountRepository loads return undefined account if email does not  exists

--- tests/infra/postgres/repo/user-account.spec.ts
+++ tests/infra/postgres/repo/user-account.spec.ts
@@ -40,17 +40,30 @@ describe('PgUserAccountRepository', () => {
         type: 'postgres',
         entities: [PgUser]
       })
-
-      // create schema
       await connection.synchronize()
       const pgUserRepo = getRepository(PgUser)
       await pgUserRepo.save({ email: 'existing_email' })
-      // => you now can use your typeorm connection !
       const sut = new PgUserAccountRepository()
 
       const account = await sut.load({ email: 'existing_email' })
 
       expect(account).toEqual({ id: '1' })
+      await connection.close()
+    })
+
+    it('Should return undefined if email does not exists', async () => {
+      const db = newDb()
+      const connection = await db.adapters.createTypeormConnection({
+        type: 'postgres',
+        entities: [PgUser]
+      })
+      await connection.synchronize()
+      const sut = new PgUserAccountRepository()
+
+      const account = await sut.load({ email: 'new_email' })
+
+      expect(account).toBeUndefined()
+      await connection.close()
     })
   })
 })
