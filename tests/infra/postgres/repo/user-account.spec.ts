7566bceb1f9ca05aa6cdc7df88d5391dad3bcaa1
Alyson Souza <alysonforever@gmail.com>
2021-07-21 10:31:21 -0300

    refactor: move inMemoryDb creation to its own file

--- tests/infra/postgres/repo/user-account.spec.ts
+++ tests/infra/postgres/repo/user-account.spec.ts
@@ -1,36 +1,10 @@
-import { LoadUserAccountRepository } from '@/data/contracts/repos'
-import { Entity, PrimaryGeneratedColumn, Column, getRepository, getConnection, Repository } from 'typeorm'
 
-import { newDb, IBackup } from 'pg-mem'
+import { PgUserAccountRepository } from '@/infra/postgres/repos'
+import { PgUser } from '@/infra/postgres/entities'
+import { makeFaceDb } from '@/tests/infra/postgres/mocks'
 
-class PgUserAccountRepository implements LoadUserAccountRepository {
-  async load (params: LoadUserAccountRepository.Params): Promise<LoadUserAccountRepository.Result> {
-    const pgUserRepo = getRepository(PgUser)
-    const pgUser = await pgUserRepo.findOne({ email: params.email })
-    if (pgUser !== undefined) {
-      return {
-        id: pgUser.id.toString(),
-        name: pgUser.name ?? undefined
-      }
-    }
-    return undefined
-  }
-}
-
-@Entity({ name: 'usuarios' })
-class PgUser {
-  @PrimaryGeneratedColumn()
-  id!: number
-
-  @Column({ name: 'nome', nullable: true })
-  name?: string
-
-  @Column()
-  email!: string
-
-  @Column({ name: 'id_facebook', nullable: true })
-  facebookId?: number
-}
+import { getRepository, getConnection, Repository } from 'typeorm'
+import { IBackup } from 'pg-mem'
 
 describe('PgUserAccountRepository', () => {
   describe('load', () => {
@@ -39,12 +13,7 @@ describe('PgUserAccountRepository', () => {
     let backup: IBackup
 
     beforeAll(async () => {
-      const db = newDb()
-      const connection = await db.adapters.createTypeormConnection({
-        type: 'postgres',
-        entities: [PgUser]
-      })
-      await connection.synchronize()
+      const db = await makeFaceDb([PgUser])
       backup = db.backup()
       pgUserRepo = getRepository(PgUser)
     })
