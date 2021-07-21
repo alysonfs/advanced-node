61719cb34379f13719853d36c07c1a82ea880ce2
Alyson Souza <alysonforever@gmail.com>
2021-07-21 10:53:04 -0300

    ensure PgUserAccountRepository create an acconut if id is undefined

--- tests/infra/postgres/repo/user-account.spec.ts
+++ tests/infra/postgres/repo/user-account.spec.ts
@@ -42,19 +42,16 @@ describe('PgUserAccountRepository', () => {
     })
   })
 
-  describe('save', () => {
-    it('Should return an account if email exists', async () => {
-      await pgUserRepo.save({ email: 'any_email' })
+  describe('saveWithFacebook', () => {
+    it('Should create an account if id is undedined', async () => {
+      await sut.saveWithFacebook({
+        email: 'any_email',
+        name: 'any_name',
+        facebookId: 'any_fb_id'
+      })
+      const pgUser = await pgUserRepo.findOne({ email: 'any_email' })
 
-      const account = await sut.load({ email: 'any_email' })
-
-      expect(account).toEqual({ id: '1' })
-    })
-
-    it('Should return undefined if email does not exists', async () => {
-      const account = await sut.load({ email: 'any_email' })
-
-      expect(account).toBeUndefined()
+      expect(pgUser?.id).toBe(1)
     })
   })
 })
