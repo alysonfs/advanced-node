2e20caefb6e0dfd482479759c54f8a94e56dd833
Alyson Souza <alysonforever@gmail.com>
2021-07-28 14:35:42 -0300

    transform ExpressRouter to a function

--- src/main/factories/controllers/facebook-login.ts
+++ src/main/factories/controllers/facebook-login.ts
@@ -1,6 +1,6 @@
 import { FacebookLoginController } from '@/application/controllers'
 import { makeFacebookAuthenticationService } from '@/main/factories/services'
 
-export const makeFacebookController = (): FacebookLoginController => {
+export const makeFacebookLoginController = (): FacebookLoginController => {
   return new FacebookLoginController(makeFacebookAuthenticationService())
 }
