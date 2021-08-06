ea47d89a92558e14c4e5b61da6c9d2929fbe686a
Alyson Souza <alysonforever@gmail.com>
2021-08-06 10:03:25 -0300

    rename JwtRokenGenerator to JwtTokenHandler

--- src/main/factories/use-cases/facebook-authentication.ts
+++ src/main/factories/use-cases/facebook-authentication.ts
@@ -1,11 +1,11 @@
 import { setupFacebookAuthentication, FacebookAuthentication } from '@/domain/use-cases'
 import { makeFacebookApi } from '@/main/factories/apis'
 import { makePgUserAccountRepository } from '@/main/factories/repos'
-import { makeJwtTokenGenerator } from '@/main/factories/crypto'
+import { makeJwtTokenHandler } from '@/main/factories/crypto'
 
 export const makeFacebookAuthentication = (): FacebookAuthentication => {
   return setupFacebookAuthentication(
     makeFacebookApi(),
     makePgUserAccountRepository(),
-    makeJwtTokenGenerator())
+    makeJwtTokenHandler())
 }
