adad012440ca136f3866b9cfe1c2733575c446d7
Alyson Souza <alysonforever@gmail.com>
2021-08-11 17:58:29 -0300

    ensure ChangeProfilePicture call SaveUserPicture with correct input

--- tests/domain/use-cases/change-profile-picture.spec.ts
+++ tests/domain/use-cases/change-profile-picture.spec.ts
@@ -19,6 +19,7 @@ describe('ChangeProfilePicture', () => {
     fileStorage.upload.mockResolvedValue('any_url')
     crypto = mock()
     userProfileRepo = mock()
+    userProfileRepo.load.mockResolvedValue({ name: 'Alyson Felipe Souza' })
     crypto.uuid.mockReturnValue(uuid)
   })
 
@@ -49,7 +50,43 @@ describe('ChangeProfilePicture', () => {
   it('Should call SaveUserPicture with correct input when file is undefined', async () => {
     await sut({ userId: 'any_id', file: undefined })
 
-    expect(userProfileRepo.savePicture).toHaveBeenCalledWith({ pictureUrl: undefined })
+    expect(userProfileRepo.savePicture).toHaveBeenCalledWith({ pictureUrl: undefined, initials: 'AS' })
+    expect(userProfileRepo.savePicture).toHaveBeenCalledTimes(1)
+  })
+
+  it('Should call SaveUserPicture with correct input when letters is lower case', async () => {
+    userProfileRepo.load.mockResolvedValueOnce({ name: 'alyson Felipe souza' })
+
+    await sut({ userId: 'any_id', file: undefined })
+
+    expect(userProfileRepo.savePicture).toHaveBeenCalledWith({ pictureUrl: undefined, initials: 'AS' })
+    expect(userProfileRepo.savePicture).toHaveBeenCalledTimes(1)
+  })
+
+  it('Should call SaveUserPicture with correct input when name with first name only', async () => {
+    userProfileRepo.load.mockResolvedValueOnce({ name: 'alyson' })
+
+    await sut({ userId: 'any_id', file: undefined })
+
+    expect(userProfileRepo.savePicture).toHaveBeenCalledWith({ pictureUrl: undefined, initials: 'AL' })
+    expect(userProfileRepo.savePicture).toHaveBeenCalledTimes(1)
+  })
+
+  it('Should call SaveUserPicture with correct input when name with one letter only', async () => {
+    userProfileRepo.load.mockResolvedValueOnce({ name: 'a' })
+
+    await sut({ userId: 'any_id', file: undefined })
+
+    expect(userProfileRepo.savePicture).toHaveBeenCalledWith({ pictureUrl: undefined, initials: 'A' })
+    expect(userProfileRepo.savePicture).toHaveBeenCalledTimes(1)
+  })
+
+  it('Should call SaveUserPicture with correct input when name with one letter only', async () => {
+    userProfileRepo.load.mockResolvedValueOnce({ name: undefined })
+
+    await sut({ userId: 'any_id', file: undefined })
+
+    expect(userProfileRepo.savePicture).toHaveBeenCalledWith({ pictureUrl: undefined, initials: undefined })
     expect(userProfileRepo.savePicture).toHaveBeenCalledTimes(1)
   })
 
