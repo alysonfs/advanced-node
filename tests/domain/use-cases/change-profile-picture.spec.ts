ab5d7af228030443a05320050557226aacb1a063
Alyson Souza <alysonforever@gmail.com>
2021-08-11 15:55:13 -0300

    ensure ChangeProfilePicture calls UploadFiles with unique key id

--- tests/domain/use-cases/change-profile-picture.spec.ts
+++ tests/domain/use-cases/change-profile-picture.spec.ts
@@ -1,11 +1,11 @@
 import { mock } from 'jest-mock-extended'
 
-type Setup = (fileStorage: UploadFile) => ChangeProfilePicture // SETUP é como se fosse o contrutor de uma classe
+type Setup = (fileStorage: UploadFile, crypto: UUIDGenerator) => ChangeProfilePicture // SETUP é como se fosse o contrutor de uma classe
 type Input = { userId: string, file: Buffer }
 type ChangeProfilePicture = (input: Input) => Promise<void>
 
-const setupChangeProfilePicture: Setup = fileStorage => async ({ userId, file }) => {
-  await fileStorage.upload({ file, key: userId })
+const setupChangeProfilePicture: Setup = (fileStorage, crypto) => async ({ userId, file }) => {
+  await fileStorage.upload({ file, key: crypto.uuid({ key: userId }) })
 }
 
 interface UploadFile {
@@ -16,14 +16,27 @@ namespace UploadFile {
   export type Input = { file: Buffer, key: string }
 }
 
+interface UUIDGenerator {
+  uuid: (input: UUIDGenerator.Input) => UUIDGenerator.Output
+}
+
+namespace UUIDGenerator {
+  export type Input = { key: string }
+  export type Output = string
+}
+
 describe('ChangeProfilePicture', () => {
   it('Should call UploadFile with correct input', async () => {
+    const uuid = 'any_unique_id'
     const file = Buffer.from('any_buffer')
     const fileStorage = mock<UploadFile>()
-    const sut = setupChangeProfilePicture(fileStorage)
+    const crypto = mock<UUIDGenerator>()
+    crypto.uuid.mockReturnValue(uuid)
+    const sut = setupChangeProfilePicture(fileStorage, crypto)
 
     await sut({ userId: 'any_id', file })
 
-    expect(fileStorage.upload).toHaveBeenCalledWith({ file, key: 'any_id' })
+    expect(fileStorage.upload).toHaveBeenCalledWith({ file, key: uuid })
+    expect(fileStorage.upload).toHaveBeenCalledTimes(1)
   })
 })
