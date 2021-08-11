18ea5881c0e3ab4756f516c86509b2825b94a30d
Alyson Souza <alysonforever@gmail.com>
2021-08-11 16:23:59 -0300

    isolate SUT creation

--- tests/domain/use-cases/change-profile-picture.spec.ts
+++ tests/domain/use-cases/change-profile-picture.spec.ts
@@ -1,4 +1,4 @@
-import { mock } from 'jest-mock-extended'
+import { mock, MockProxy } from 'jest-mock-extended'
 
 type Setup = (fileStorage: UploadFile, crypto: UUIDGenerator) => ChangeProfilePicture // SETUP é como se fosse o contrutor de uma classe
 type Input = { userId: string, file: Buffer }
@@ -26,14 +26,25 @@ namespace UUIDGenerator {
 }
 
 describe('ChangeProfilePicture', () => {
-  it('Should call UploadFile with correct input', async () => {
-    const uuid = 'any_unique_id'
-    const file = Buffer.from('any_buffer')
-    const fileStorage = mock<UploadFile>()
-    const crypto = mock<UUIDGenerator>()
-    crypto.uuid.mockReturnValue(uuid)
-    const sut = setupChangeProfilePicture(fileStorage, crypto)
+  let uuid: string
+  let file: Buffer
+  let fileStorage: MockProxy<UploadFile>
+  let crypto: MockProxy<UUIDGenerator>
+  let sut: ChangeProfilePicture
 
+  beforeAll(async () => {
+    uuid = 'any_unique_id'
+    file = Buffer.from('any_buffer')
+    fileStorage = mock()
+    crypto = mock()
+    crypto.uuid.mockReturnValue(uuid)
+  })
+
+  beforeEach(async () => {
+    sut = setupChangeProfilePicture(fileStorage, crypto)
+  })
+
+  it('Should call UploadFile with correct input', async () => {
     await sut({ userId: 'any_id', file })
 
     expect(fileStorage.upload).toHaveBeenCalledWith({ file, key: uuid })
