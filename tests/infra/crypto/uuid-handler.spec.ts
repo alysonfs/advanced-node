6043eac8411598b1b90fcaa818ea9ac6224ef058
Alyson Souza <alysonforever@gmail.com>
2021-08-18 17:57:23 -0300

    isolate SUT creation

--- tests/infra/crypto/uuid-handler.spec.ts
+++ tests/infra/crypto/uuid-handler.spec.ts
@@ -12,18 +12,23 @@ class UUIDHandler {
 }
 
 describe('UUIDHandler', () => {
-  it('Should call uuid.v4', async () => {
-    const sut = new UUIDHandler()
+  let sut: UUIDHandler
 
+  beforeAll(() => {
+    sut = new UUIDHandler()
+  })
+
+  beforeEach(() => {
+    mocked(v4).mockReturnValue('any_uuid')
+  })
+
+  it('Should call uuid.v4', async () => {
     sut.uuid({ key: 'any_key' })
 
     expect(v4).toHaveBeenCalledTimes(1)
   })
 
   it('Should return correct uuid', async () => {
-    mocked(v4).mockReturnValueOnce('any_uuid')
-    const sut = new UUIDHandler()
-
     const uuid = sut.uuid({ key: 'any_key' })
 
     expect(uuid).toBe('any_key_any_uuid')
