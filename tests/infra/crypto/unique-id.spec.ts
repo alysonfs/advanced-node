d9d6df7dff6e580e0ae7e585c4a513d6c636855d
Alyson Souza <alysonforever@gmail.com>
2021-08-19 09:15:03 -0300

    move UniqueId to its own file

--- tests/infra/crypto/unique-id.spec.ts
+++ tests/infra/crypto/unique-id.spec.ts
@@ -1,25 +1,8 @@
-import { UUIDGenerator } from '@/domain/contracts/gateways'
+import { UniqueId } from '@/infra/crypto'
 
-class UniqueID implements UUIDGenerator {
-  constructor (
-    private readonly date: Date
-  ) { }
-
-  uuid ({ key }: UUIDGenerator.Input): UUIDGenerator.Output {
-    return key +
-      '_' +
-      this.date.getFullYear().toString() +
-      (this.date.getMonth() + 1).toString().padStart(2, '0') +
-      this.date.getDate().toString().padStart(2, '0') +
-      this.date.getHours().toString().padStart(2, '0') +
-      this.date.getMinutes().toString().padStart(2, '0') +
-      this.date.getSeconds().toString().padStart(2, '0')
-  }
-}
-
-describe('UniqueID', () => {
+describe('UniqueId', () => {
   it('Should call uuid', async () => {
-    const sut = new UniqueID(new Date(2021, 9, 3, 10, 10, 10, 10))
+    const sut = new UniqueId(new Date(2021, 9, 3, 10, 10, 10, 10))
 
     const uuid = sut.uuid({ key: 'any_key' })
 
@@ -27,7 +10,7 @@ describe('UniqueID', () => {
   })
 
   it('Should call uuid', async () => {
-    const sut = new UniqueID(new Date(2018, 2, 10, 18, 1, 0))
+    const sut = new UniqueId(new Date(2018, 2, 10, 18, 1, 0))
 
     const uuid = sut.uuid({ key: 'any_key' })
 
