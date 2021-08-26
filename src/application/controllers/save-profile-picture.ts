c42d33a80606bc07900d21aa61803f6d40bb1dd1
Alyson Souza <alysonforever@gmail.com>
2021-08-26 18:44:36 -0300

    change controller to use validators

--- src/application/controllers/save-profile-picture.ts
+++ src/application/controllers/save-profile-picture.ts
@@ -1,7 +1,7 @@
 import { ChangeProfilePicture } from '@/domain/use-cases'
-import { badRequest, HttpResponse, ok } from '@/application/helpers'
-import { RequiredFieldError, InvalidMimeTypeError, MaxFileSizeError } from '@/application/errors'
 import { Controller } from './controller'
+import { AllowedMimeType, MaxFileSize, Required, RequiredBuffer, Validator } from '@/application/validation'
+import { HttpResponse, ok } from '@/application/helpers'
 
 type HttpRequest = { file: { buffer: Buffer, mimeType: string }, userId: string }
 type Model = Error | { pictureUrl?: string, initials?: string }
@@ -11,12 +11,16 @@ export class SavePictureController extends Controller {
     super()
   }
 
-  async perform ({ file, userId }: HttpRequest): Promise<HttpResponse<Model>> {
-    if (file === undefined || file === null) return badRequest(new RequiredFieldError('file'))
-    if (file.buffer.length === 0) return badRequest(new RequiredFieldError('file'))
-    if (!['image/png', 'image/jpeg', 'image/jpg'].includes(file.mimeType)) return badRequest(new InvalidMimeTypeError(['png', 'jpeg', 'jpg']))
-    if (file.buffer.length > 5 * 1024 * 1024) return badRequest(new MaxFileSizeError(5))
+  override async perform ({ file, userId }: HttpRequest): Promise<HttpResponse<Model>> {
     const data = await this.changeProfilePicture({ userId, file: file.buffer })
     return ok(data)
+  }
+
+  override buildValidators ({ file }: HttpRequest): Validator[] {
+    return [
+      new Required(file, 'file'),
+      new RequiredBuffer(file.buffer, 'file'),
+      new AllowedMimeType(['jpg', 'png'], file.mimeType),
+      new MaxFileSize(5, file.buffer)]
   }
 }
